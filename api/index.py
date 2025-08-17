a012d6db78c1608716433d3dbf2d9a29335c73bc
Narukisora <ziolesbloxs@gmail.com>
2025-08-17 19:18:12 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+# tell Flask where to find templates (important for Vercel’s file layout)
+app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), '../templates'))
 
 @app.route("/")
 def home():
     return render_template("index.html")
-
-# DO NOT put app.run() here
-# Vercel will automatically detect "app"
